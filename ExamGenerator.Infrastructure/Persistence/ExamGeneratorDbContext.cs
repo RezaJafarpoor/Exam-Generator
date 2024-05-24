8d4808b60222794a4b2c0f98bab13e1c158f224f
reza <rezajafarpoor222@gmail.com>
2024-05-24 23:41:12 +0330

    adding DbSet's

---
+++ ExamGenerator.Infrastructure/Persistence/ExamGeneratorDbContext.cs
@@ -0,0 +1,16 @@
+﻿using ExamGenerator.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExamGenerator.Infrastructure.Persistence;
+
+internal class ExamGeneratorDbContext(DbContextOptions options) : DbContext
+{
+
+    internal DbSet<Customer> Customers;
+    internal DbSet<Exam> Exams;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
